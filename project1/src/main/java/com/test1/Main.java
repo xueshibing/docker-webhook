88fd83b333f2456b1f5073a562d44d47232da42c
xueshibing <xueshibing@gmail.com>
2016-02-04 15:15:15 +0800

    a

---
+++ project1/src/main/java/com/test1/Main.java
@@ -0,0 +1,10 @@
+package com.test1;
+
+/**
+ * Created by kevin on 2/3/16.
+ */
+public class Main {
+    public static void say(String s){
+        System.out.println(s);
+    }
+}
--- project2/src/main/java/com/Hello.java
+++ project2/src/main/java/com/Hello.java
@@ -1,4 +1,4 @@
-package test;
+package com;
 
 public class Hello {
   public static void main(String[] args) {
